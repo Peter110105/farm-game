6b5926e96315dc4014cdbdf8893a31e16935b89c
Peter110105 <pj40228@gmail.com>
2025-08-13 15:27:09 +0800

    feat: 重構背包組件，更新UI畫面

--- src/app/entities/inventory/inventory.component.ts
+++ src/app/entities/inventory/inventory.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InventoryService } from './service/inventory.service';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { GameDataService } from '../../core/game-data/game-data.service';
 
 @Component({
   selector: 'app-inventory',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NzDrawerModule, NzButtonModule],
   templateUrl: './inventory.component.html',
   styleUrls: ['./inventory.component.css']
 })
 export class InventoryComponent {
-  isOpen = false;
-  constructor(public inventoryService: InventoryService) {}
+  visible = false;
+  constructor(public inventoryService: InventoryService, public gameDataService: GameDataService) {}
   
   toggle(): void{
-    this.isOpen = !this.isOpen;
+    this.visible = !this.visible;
   }
 
   upgrade(): void {
